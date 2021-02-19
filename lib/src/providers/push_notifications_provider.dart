fab1240eecbfbd4a34fdf176549e207134c4dee1
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-02-19 07:20:39 -0500

    Recibir Notificacion

--- lib/src/providers/push_notifications_provider.dart
+++ lib/src/providers/push_notifications_provider.dart
@@ -22,16 +22,23 @@ class PushNotificationsProvider {
   Future<dynamic> onMessage(Map<String, dynamic> message) async {
     print("==== onMessage ====");
     print("message: $message");
+
+    final argumento = message['data']['hobby'];
+    print(argumento);
   }
 
   Future<dynamic> onLaunch(Map<String, dynamic> message) async {
     print("==== onLaunch ====");
     print("message: $message");
+    final argumento = message['data']['hobby'];
+    print(argumento);
   }
 
   Future<dynamic> onResume(Map<String, dynamic> message) async {
     print("==== onResume ====");
     print("message: $message");
+    final argumento = message['data']['hobby'];
+    print(argumento);
   }
 
   initNotifications() async {
